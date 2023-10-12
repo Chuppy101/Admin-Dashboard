1c214c4b64330f47fa28be363406db9a508d520f
Chuppy101 <Very_vazhno@mail.ru>
2023-10-12 20:10:17 +0300

    Project finished

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<span>Admin panel</span>
+			<span>©2023 Admin panel, All rights reserved </span>
+		</div>
+	)
+}
+
+export default Footer
---
+++ src/data.ts
@@ -0,0 +1,721 @@
+export const menu = [
+	{
+		id: 1,
+		title: "MAIN",
+		listItems: [
+			{
+				id: 1,
+				title: "Homepage",
+				url: "/",
+				icon: "home.svg",
+			},
+			{
+				id: 2,
+				title: "Profile",
+				url: "/users/1",
+				icon: "user.svg",
+			},
+		],
+	},
+	{
+		id: 2,
+		title: "LISTS",
+		listItems: [
+			{
+				id: 1,
+				title: "Users",
+				url: "/users",
+				icon: "user.svg",
+			},
+			{
+				id: 2,
+				title: "Products",
+				url: "/products",
+				icon: "product.svg",
+			},
+			{
+				id: 3,
+				title: "Orders",
+				url: "/orders",
+				icon: "order.svg",
+			},
+			{
+				id: 4,
+				title: "Posts",
+				url: "/posts",
+				icon: "post2.svg",
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "GENERAL",
+		listItems: [
+			{
+				id: 1,
+				title: "Elements",
+				url: "/",
+				icon: "element.svg",
+			},
+			{
+				id: 2,
+				title: "Notes",
+				url: "/",
+				icon: "note.svg",
+			},
+			{
+				id: 3,
+				title: "Forms",
+				url: "/",
+				icon: "form.svg",
+			},
+			{
+				id: 4,
+				title: "Calendar",
+				url: "/posts",
+				icon: "calendar.svg",
+			},
+		],
+	},
+	{
+		id: 4,
+		title: "MAINTENANCE",
+		listItems: [
+			{
+				id: 1,
+				title: "Settings",
+				url: "/",
+				icon: "setting.svg",
+			},
+			{
+				id: 2,
+				title: "Backups",
+				url: "/",
+				icon: "backup.svg",
+			},
+		],
+	},
+	{
+		id: 5,
+		title: "ANALYTICS",
+		listItems: [
+			{
+				id: 1,
+				title: "Charts",
+				url: "/",
+				icon: "chart.svg",
+			},
+			{
+				id: 2,
+				title: "Logs",
+				url: "/",
+				icon: "log.svg",
+			},
+		],
+	},
+]
+
+export const topDealUsers = [
+	{
+		id: 1,
+		img: "https://media.istockphoto.com/id/1278976856/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D1%87%D0%B0%D1%81%D1%82%D0%BB%D0%B8%D0%B2%D0%B0%D1%8F-%D0%B4%D0%B5%D0%B2%D0%BE%D1%87%D0%BA%D0%B0-%D0%B2-%D1%81%D1%80%D0%B5%D0%B4%D0%BD%D0%B5%D0%B9-%D1%88%D0%BA%D0%BE%D0%BB%D0%B5.jpg?s=612x612&w=0&k=20&c=0G3FVQqH6WNvfdbZBIYdU3m16RUfa4-Ozq90gcH_iNA=",
+		username: "Elva McDonald",
+		email: "elva@gmail.com",
+		amount: "3.668",
+	},
+	{
+		id: 2,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4xXjkrEjETy6JO8_oFmDVvRIO9rS-b7wSEA&usqp=CAU",
+		username: "Linnie Nelson",
+		email: "linnie@gmail.com",
+		amount: "3.256",
+	},
+	{
+		id: 3,
+		img: "https://media.istockphoto.com/id/1331257339/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D1%81%D1%87%D0%B0%D1%81%D1%82%D0%BB%D0%B8%D0%B2%D0%BE%D0%B3%D0%BE-%D0%B7%D1%80%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE-%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD%D1%8B-%D0%B2-%D0%BE%D1%87%D0%BA%D0%B0%D1%85-%D0%B8-%D1%81%D0%BC%D0%BE%D1%82%D1%80%D1%8F%D1%89%D0%B5%D0%B3%D0%BE-%D0%B2-%D0%BA%D0%B0%D0%BC%D0%B5%D1%80%D1%83-%D0%B2-%D0%BF%D0%BE%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D0%B8.jpg?s=612x612&w=0&k=20&c=m9fasyZwElY6KtNBK6hvhVAWtvcL1dtKikYKhQErVhE=",
+		username: "Brent Reeves",
+		email: "brent@gmail.com",
+		amount: "2.998",
+	},
+	{
+		id: 4,
+		img: "https://img.freepik.com/free-photo/lifestyle-different-emotions-leisure-activities-concept-confident-beautiful-young-woman-say-no-problem-guarantee-quality-show-okay-sign-tilt-head-and-smiling-pleased-satisfied-good-result_1258-57762.jpg",
+		username: "Adeline Watson",
+		email: "adeline@gmail.com",
+		amount: "2.512",
+	},
+	{
+		id: 5,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMuXMBxHLzsuZiXYWOHH8QTX8apoi_g6AVAQ&usqp=CAU",
+		username: "Juan Harrington",
+		email: "juan@gmail.com",
+		amount: "2.134",
+	},
+	{
+		id: 6,
+		img: "https://media.istockphoto.com/id/1220976906/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D0%B0%D1%8F-%D1%83%D0%BB%D1%8B%D0%B1%D0%B0%D1%8E%D1%89%D0%B0%D1%8F%D1%81%D1%8F-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D0%B0-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D1%8B-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5.jpg?s=612x612&w=0&k=20&c=ACS-f_U98MvwR8POh25AA4otcCGmfd2FWmItdECDZbQ=",
+		username: "Augusta McGee",
+		email: "augusta@gmail.com",
+		amount: "1.932",
+	},
+	{
+		id: 7,
+		img: "https://img.freepik.com/free-photo/handsome-man-standing-in-the-street-and-holding-laptop_1303-31617.jpg",
+		username: "Angel Thomas",
+		email: "angel@gmail.com",
+		amount: "1.560",
+	},
+]
+
+export const chartBoxUser = {
+	color: "#8884d8",
+	icon: "/userIcon.svg",
+	title: "Total Users",
+	number: "11.238",
+	dataKey: "users",
+	percentage: 45,
+	chartData: [
+		{ name: "Sun", users: 400 },
+		{ name: "Mon", users: 600 },
+		{ name: "Tue", users: 500 },
+		{ name: "Wed", users: 700 },
+		{ name: "Thu", users: 400 },
+		{ name: "Fri", users: 500 },
+		{ name: "Sat", users: 450 },
+	],
+}
+
+export const chartBoxProduct = {
+	color: "skyblue",
+	icon: "/productIcon.svg",
+	title: "Total Products",
+	number: "238",
+	dataKey: "products",
+	percentage: 21,
+	chartData: [
+		{ name: "Sun", products: 400 },
+		{ name: "Mon", products: 600 },
+		{ name: "Tue", products: 500 },
+		{ name: "Wed", products: 700 },
+		{ name: "Thu", products: 400 },
+		{ name: "Fri", products: 500 },
+		{ name: "Sat", products: 450 },
+	],
+}
+
+export const chartBoxRevenue = {
+	color: "teal",
+	icon: "/revenueIcon.svg",
+	title: "Total Revenue",
+	number: "$56.432",
+	dataKey: "revenue",
+	percentage: -12,
+	chartData: [
+		{ name: "Sun", revenue: 400 },
+		{ name: "Mon", revenue: 600 },
+		{ name: "Tue", revenue: 500 },
+		{ name: "Wed", revenue: 700 },
+		{ name: "Thu", revenue: 400 },
+		{ name: "Fri", revenue: 500 },
+		{ name: "Sat", revenue: 450 },
+	],
+}
+
+export const chartBoxConversion = {
+	color: "gold",
+	icon: "/conversionIcon.svg",
+	title: "Total Ratio",
+	number: "2.6",
+	dataKey: "ratio",
+	percentage: 12,
+	chartData: [
+		{ name: "Sun", ratio: 400 },
+		{ name: "Mon", ratio: 600 },
+		{ name: "Tue", ratio: 500 },
+		{ name: "Wed", ratio: 700 },
+		{ name: "Thu", ratio: 400 },
+		{ name: "Fri", ratio: 500 },
+		{ name: "Sat", ratio: 450 },
+	],
+}
+
+export const barChartBoxRevenue = {
+	title: "Profit Earned",
+	color: "#8884d8",
+	dataKey: "profit",
+	chartData: [
+		{
+			name: "Sun",
+			profit: 4000,
+		},
+		{
+			name: "Mon",
+			profit: 3000,
+		},
+		{
+			name: "Tue",
+			profit: 2000,
+		},
+		{
+			name: "Wed",
+			profit: 2780,
+		},
+		{
+			name: "Thu",
+			profit: 1890,
+		},
+		{
+			name: "Fri",
+			profit: 2390,
+		},
+		{
+			name: "Sat",
+			profit: 3490,
+		},
+	],
+}
+
+export const barChartBoxVisit = {
+	title: "Total Visit",
+	color: "#FF8042",
+	dataKey: "visit",
+	chartData: [
+		{
+			name: "Sun",
+			visit: 4000,
+		},
+		{
+			name: "Mon",
+			visit: 3000,
+		},
+		{
+			name: "Tue",
+			visit: 2000,
+		},
+		{
+			name: "Wed",
+			visit: 2780,
+		},
+		{
+			name: "Thu",
+			visit: 1890,
+		},
+		{
+			name: "Fri",
+			visit: 2390,
+		},
+		{
+			name: "Sat",
+			visit: 3490,
+		},
+	],
+}
+
+export const userRows = [
+	{
+		id: 1,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUiByTPtaQEIMkM3Oltmbi4-twNEvMZay8ew&usqp=CAU",
+		lastName: "Hubbard",
+		firstName: "Eula",
+		email: "kewez@@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 2,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhtFAJq5s7NWZyB0vuhbEVPrcdyXphXC9ELQ&usqp=CAU",
+		lastName: "Manning",
+		firstName: "Stella",
+		email: "comhuhmit@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 3,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8bFHLFJGCEEOYkIY8Qt6MAoEFM62n7pGCSw&usqp=CAU",
+		lastName: "Greer",
+		firstName: "Mary",
+		email: "ujudokon@hottmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 4,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSBEf6HXaJrEd1xtgGX-1VeVAatNGPcQINVA&usqp=CAU",
+		lastName: "Williamson",
+		firstName: "Mildred",
+		email: "tinhavabe@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 5,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlKl0tzUYzvQS5lkNXpllHXTCAshSLyCE33Q&usqp=CAU",
+		lastName: "Gross",
+		firstName: "Jose",
+		email: "gobtagbes@yahoo.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 6,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrAGjG5406PqCJPHFfFzZ6VCnykYWYjgF8Hw&usqp=CAU",
+		lastName: "Sharp",
+		firstName: "Jeremy",
+		email: "vulca.eder@mail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 7,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqKHd90o5IUM_hO_b5cJOirX3itL10oKE-IA&usqp=CAU",
+		lastName: "Lowe",
+		firstName: "Christina",
+		email: "reso.bilic@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 8,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX9OPJzmgOpEO6Q7rSAl-PvIyVwhhYrQlNMg&usqp=CAU",
+		lastName: "Dean",
+		firstName: "Garrett",
+		email: "codaic@mail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 9,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSWKrn9cQmN6W394pUPmuHM-HczM0gncRtlg&usqp=CAU",
+		lastName: "Parsons",
+		firstName: "Leah",
+		email: "uzozor@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 10,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtYI7JzzEyiCrLHlBDGejDva7K8xRJ3LUlhg&usqp=CAU",
+		lastName: "Reid",
+		firstName: "Elnora",
+		email: "tuhkabapu@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 11,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYOM5aEri-1utf3-z0-GMVqBoeZK8Ix3y8vA&usqp=CAU",
+		lastName: "Dunn",
+		firstName: "Gertrude",
+		email: "gibo@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+		verified: true,
+	},
+	{
+		id: 12,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTO9_01SZ7hFnlt1TGOQG_pQ1xe0PY2R9E9A&usqp=CAU",
+		lastName: "Williams",
+		firstName: "Mark",
+		email: "tic.harvey@hotmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 13,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HP2EmAXlxBqL3iLgA-Hdhb1HzJL_RpFyDg&usqp=CAU",
+		lastName: "Cruz",
+		firstName: "Charlotte",
+		email: "ceuc@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 14,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDV0XHcwxkJtL0nPJNgojk9-_Tu81RJ3WmCg&usqp=CAU",
+		lastName: "Harper",
+		firstName: "Sara",
+		email: "bafuv@hotmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 15,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhl8Rv7dHipxtgRwHsCfZa0jEeGc5I4Ej9PQ&usqp=CAU",
+		lastName: "Griffin",
+		firstName: "Eric",
+		email: "ubi@gmail.com",
+		phone: "123 456 789",
+		createdAt: "01.02.2023",
+	},
+]
+
+export const products = [
+	{
+		id: 1,
+		img: "https://store.sony.com.au/on/demandware.static/-/Sites-sony-master-catalog/default/dw1b537bbb/images/PLAYSTATION5W/PLAYSTATION5W.png",
+		title: "Playstation 5 Digital Edition",
+		color: "white",
+		producer: "Sony",
+		price: "$250.99",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 2,
+		img: "https://www.pngmart.com/files/6/Dell-Laptop-PNG-Image.png",
+		title: "Dell Laptop KR211822",
+		color: "black",
+		producer: "Dell",
+		price: "$499.99",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 3,
+		img: "http://images.samsung.com/is/image/samsung/uk-led-tv-hg40ed670ck-hg40ed670ckxxu-001-front",
+		title: "Samsung TV 4K SmartTV",
+		color: "gray",
+		producer: "Samsung",
+		price: "$999.49",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 4,
+		img: "https://raylo.imgix.net/iphone-14-blue.png",
+		title: "Apple Iphone 14 Pro Max",
+		color: "white",
+		producer: "Apple",
+		price: "$799.49",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 5,
+		img: "https://www.signify.com/b-dam/signify/en-aa/about/news/2020/20200903-movie-night-essentials-popcorn-ice-cream-and-the-new-philips-hue-play-gradient-lightstrip/packaging-lighstrip.png",
+		title: "Philips Hue Play Gradient",
+		color: "rainbow",
+		producer: "Philips",
+		price: "$39.99",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 6,
+		img: "https://www.smartworld.it/wp-content/uploads/2019/09/High_Resolution_PNG-MX-Master-3-LEFT-GRAPHITE.png",
+		title: "Logitech MX Master 3",
+		color: "black",
+		producer: "Logitech",
+		price: "$59.49",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 7,
+		img: "https://www.pngarts.com/files/7/Podcast-Mic-PNG-Picture.png",
+		title: "Rode Podcast Microphone",
+		color: "gray",
+		producer: "Rode",
+		price: "$119.49",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 8,
+		img: "https://5.imimg.com/data5/SW/VM/MY-5774620/toshiba-split-ac-2-ton-3-star-rated-ras-24s3ks-500x500.png",
+		title: "Toshiba Split AC 2",
+		color: "white",
+		producer: "Toshiba",
+		price: "$899.99",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+	{
+		id: 9,
+		img: "https://img.productz.com/review_image/102489/preview_sony-kdl-50w800b-50-inch-hdtv-review-superb-picture-102489.png",
+		title: "Sony Bravia KDL-47W805A",
+		color: "black",
+		producer: "Sony",
+		price: "$970.49",
+		createdAt: "01.02.2023",
+	},
+	{
+		id: 10,
+		img: "https://venturebeat.com/wp-content/uploads/2015/07/As_AO1-131_gray_nonglare_win10_03.png?fit=1338%2C1055&strip=all",
+		title: "Acer Laptop 16 KL-4804",
+		color: "black",
+		producer: "Acer",
+		price: "$599.99",
+		createdAt: "01.02.2023",
+		inStock: true,
+	},
+]
+
+export const singleUser = {
+	id: 1,
+	title: "Alex Chubchik",
+	img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTO9_01SZ7hFnlt1TGOQG_pQ1xe0PY2R9E9A&usqp=CAU",
+	info: {
+		username: "Alex98",
+		fullname: "Alexandr Chubchik",
+		email: "a_chubchik@mail.ru",
+		phone: "+7 921 868 53 66",
+		status: "verified",
+	},
+	chart: {
+		dataKeys: [
+			{ name: "visits", color: "#82ca9d" },
+			{ name: "clicks", color: "#8884d8" },
+		],
+		data: [
+			{
+				name: "Sun",
+				visits: 4000,
+				clicks: 2400,
+			},
+			{
+				name: "Mon",
+				visits: 3000,
+				clicks: 1398,
+			},
+			{
+				name: "Tue",
+				visits: 2000,
+				clicks: 3800,
+			},
+			{
+				name: "Wed",
+				visits: 2780,
+				clicks: 3908,
+			},
+			{
+				name: "Thu",
+				visits: 1890,
+				clicks: 4800,
+			},
+			{
+				name: "Fri",
+				visits: 2390,
+				clicks: 3800,
+			},
+			{
+				name: "Sat",
+				visits: 3490,
+				clicks: 4300,
+			},
+		],
+	},
+	activities: [
+		{
+			text: "Alex purchased Playstation 5 Digital Edition",
+			time: "3 day ago",
+		},
+		{
+			text: "Alex added 3 items into their wishlist",
+			time: "1 week ago",
+		},
+		{
+			text: "Alex purchased Sony Bravia KD-32w800",
+			time: "2 weeks ago",
+		},
+		{
+			text: "Alex reviewed a product",
+			time: "1 month ago",
+		},
+		{
+			text: "Alex added 1 items into their wishlist",
+			time: "1 month ago",
+		},
+		{
+			text: "Alex reviewed a product",
+			time: "2 months ago",
+		},
+	],
+}
+export const singleProduct = {
+	id: 1,
+	title: "Playstation 5 Digital Edition",
+	img: "https://store.sony.com.au/on/demandware.static/-/Sites-sony-master-catalog/default/dw1b537bbb/images/PLAYSTATION5W/PLAYSTATION5W.png",
+	info: {
+		productId: "Ps5SDF1156d",
+		color: "white",
+		price: "$250.99",
+		producer: "Sony",
+		export: "Japan",
+	},
+	chart: {
+		dataKeys: [
+			{ name: "visits", color: "#82ca9d" },
+			{ name: "orders", color: "#8884d8" },
+		],
+		data: [
+			{
+				name: "Sun",
+				visits: 4000,
+				orders: 2400,
+			},
+			{
+				name: "Mon",
+				visits: 3000,
+				orders: 1398,
+			},
+			{
+				name: "Tue",
+				visits: 2000,
+				orders: 3800,
+			},
+			{
+				name: "Wed",
+				visits: 2780,
+				orders: 3908,
+			},
+			{
+				name: "Thu",
+				visits: 1890,
+				orders: 4800,
+			},
+			{
+				name: "Fri",
+				visits: 2390,
+				orders: 3800,
+			},
+			{
+				name: "Sat",
+				visits: 3490,
+				orders: 4300,
+			},
+		],
+	},
+	activities: [
+		{
+			text: "John Doe purchased Playstation 5 Digital Edition",
+			time: "3 day ago",
+		},
+		{
+			text: "Jane Doe added Playstation 5 Digital Edition into their wishlist",
+			time: "1 week ago",
+		},
+		{
+			text: "Mike Doe purchased Playstation 5 Digital Edition",
+			time: "2 weeks ago",
+		},
+		{
+			text: "Anna Doe reviewed the product",
+			time: "1 month ago",
+		},
+		{
+			text: "Michael Doe added Playstation 5 Digital Edition into their wishlist",
+			time: "1 month ago",
+		},
+		{
+			text: "Helen Doe reviewed the product",
+			time: "2 months ago",
+		},
+	],
+}
